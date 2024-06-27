14f49c6cc95fdd6baf73c3a3e5162f3fe3476441
jaf333 <jaf.attd@gmail.com>
2024-06-27 12:51:56 +0200

    implement optimizer

---
+++ neuronet/optimizer.py
@@ -0,0 +1,13 @@
+from neuronet.neural_network import NeuralNet
+
+class Optimizer:
+    def step(self, net: NeuralNet) -> None:
+        raise NotImplementedError
+
+class SGD(Optimizer):
+    def __init__(self, lr: float = 0.01) -> None:
+        self.lr = lr
+
+    def step(self, net: NeuralNet) -> None:
+        for param, grad in net.params_and_grads():
+            param -= self.lr * grad
